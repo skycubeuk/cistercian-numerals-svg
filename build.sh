4d03f62d09b6a356a23c79af7f61a847adac26db
Graeme Dyas <admin@zabouth.com>
2022-10-26 00:35:46 +0100

    Added seg display

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 php ./main.php
 svgo -f ./out -o ./out
+# inkscape 3066.svg --export-type=png --export-filename=out.png --export-dpi=100 --export-background-opacity=0.0
--- main.php
+++ main.php
@@ -9,6 +9,8 @@ function progressBar($done, $total)
 }
 
 $ds = array();
+#$inpath = "./in-seg/";
+$inpath = "./in-vec/";
 for ($i = 1; $i < 10000; $i++) {
     $a = strval($i);
     while (strlen($a) < 4) {
@@ -23,25 +25,25 @@ foreach ($ds as $key => $value) {
     # 1000s
     if ($value[0] > 0) {
         $f = $value[0] . "000.svg";
-        array_push($out, trim(file_get_contents("./in/" . $f)));
+        array_push($out, trim(file_get_contents($inpath . $f)));
     }
     #100s
     if ($value[1] > 0) {
         $f = $value[1] . "00.svg";
-        array_push($out, trim(file_get_contents("./in/" . $f)));
+        array_push($out, trim(file_get_contents($inpath . $f)));
     }
     #10s
     if ($value[2] > 0) {
         $f = $value[2] . "0.svg";
-        array_push($out, trim(file_get_contents("./in/" . $f)));
+        array_push($out, trim(file_get_contents($inpath . $f)));
     }
     #1s
     if ($value[3] > 0) {
         $f = $value[3] . ".svg";
-        array_push($out, trim(file_get_contents("./in/" . $f)));
+        array_push($out, trim(file_get_contents($inpath . $f)));
     }
     $txt = implode("\n", $out);
-    $base = file_get_contents("./in/base.svg");
+    $base = file_get_contents($inpath . "base.svg");
     $baseout = str_replace("@rep@", $txt, $base);
     file_put_contents("./out/" . $outf, $baseout);
     progressBar($key + 1, 9999);
